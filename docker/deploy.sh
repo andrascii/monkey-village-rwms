43ca619a320b3b577b99458a3399a3a082944af8
Андрей <apugachevdev@gmail.com>
2026-07-07 20:30:55 +0300

    Добавлен deploy.sh скрипт

---
+++ docker/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SSH_HOST="mi.fornex.tgbot"
+REMOTE_DIR="/srv/monkey-island/rwms"
+IMAGE_TAR_NAME="rwms-amd64.tar"
+
+ssh "${SSH_HOST}" "\
+    if [ -f '${REMOTE_DIR}/${IMAGE_TAR_NAME}' ]; then \
+        if [ -f '${REMOTE_DIR}/${IMAGE_TAR_NAME}.bak' ]; then \
+            ts=\$(date +%Y%m%d-%H%M%S); \
+            mv '${REMOTE_DIR}/${IMAGE_TAR_NAME}.bak' '${REMOTE_DIR}/${IMAGE_TAR_NAME}.'\"\${ts}\"'.bak'; \
+        fi; \
+        mv '${REMOTE_DIR}/${IMAGE_TAR_NAME}' '${REMOTE_DIR}/${IMAGE_TAR_NAME}.bak'; \
+    fi"
+
+scp "./${IMAGE_TAR_NAME}" "${SSH_HOST}:${REMOTE_DIR}"
